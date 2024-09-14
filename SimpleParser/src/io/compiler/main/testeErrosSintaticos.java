70f9c1fdbf687a68372bcf56f21fe5e898c8a91a
DiogoHiga <diogo.sadao2003@gmail.com>
2024-09-13 21:01:26 -0300

    Organiza os arquivos

--- SimpleParser/src/io/compiler/main/testeErrosSintaticos.java
+++
@@ -1,9 +0,0 @@
-package io.compiler.main;
-public class testeErrosSintaticos {
-    public static void main(String args[]) {
-        int x;
-
-        x = 10;
-        System.out.println(x);
-    }
-}
